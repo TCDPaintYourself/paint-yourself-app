bf5962c00f5dbb51a2a9a7b494db27e4aa2f0f14
moreplatesmoredates <nugentd2@tcd.ie>
2022-02-04 18:47:34 +0000

    adding absolute imports

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -10,14 +10,15 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { ColorSchemeName, Pressable } from 'react-native';
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import TabOneScreen from '../screens/TabOneScreen';
-import TabTwoScreen from '../screens/TabTwoScreen';
-import { RootStackParamList, RootTabParamList, RootTabScreenProps } from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
+import Colors from '@constants/Colors';
+import useColorScheme from '@hooks/useColorScheme';
+import ModalScreen from '@screens/ModalScreen';
+import NotFoundScreen from '@screens/NotFoundScreen';
+import TabOneScreen from '@screens/TabOneScreen';
+import TabTwoScreen from '@screens/TabTwoScreen';
+import { RootStackParamList, RootTabParamList, RootTabScreenProps } from 'types';
+import LinkingConfiguration from '@navigation/LinkingConfiguration';
+
 
 export default function Navigation({ colorScheme }: { colorScheme: ColorSchemeName }) {
   return (
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,8 +1,8 @@
 import { StyleSheet } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import { RootTabScreenProps } from '../types';
+import EditScreenInfo from '@components/EditScreenInfo';
+import { Text, View } from '@components/Themed';
+import { RootTabScreenProps } from 'types';
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
   return (
