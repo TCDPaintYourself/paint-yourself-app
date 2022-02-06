15ba249e86ef46340274f35fd099ddf2d439e109
ClaireCassidy <clairecassidy0@hotmail.com>
2022-02-06 21:08:30 +0000

    choose random bg image for login/register screen

--- screens/LoginRegisterScreen.tsx
+++ screens/LoginRegisterScreen.tsx
@@ -13,6 +13,20 @@ import { RootTabScreenProps } from "../types";
 export default function LoginRegisterScreen({
   navigation,
 }: RootTabScreenProps<"TabOne">) {
+  // select random background image
+  const NUM_BG_IMAGES = 8;
+  const images = [
+    require("../assets/images/auth_bgs/auth_background_1.jpg"),
+    require("../assets/images/auth_bgs/auth_background_2.jpg"),
+    require("../assets/images/auth_bgs/auth_background_3.jpg"),
+    require("../assets/images/auth_bgs/auth_background_4.jpg"),
+    require("../assets/images/auth_bgs/auth_background_5.jpg"),
+    require("../assets/images/auth_bgs/auth_background_6.jpg"),
+    require("../assets/images/auth_bgs/auth_background_7.jpg"),
+    require("../assets/images/auth_bgs/auth_background_8.jpg"),
+  ];
+  const bg_image_index = Math.floor(Math.random() * (NUM_BG_IMAGES - 1)) + 1;
+
   // Front-end oauth here
   const googleAuth = () => {
     navigation.navigate("Modal");
@@ -22,13 +36,13 @@ export default function LoginRegisterScreen({
     <View style={styles.container}>
       <ImageBackground
         resizeMode="cover"
-        source={require("../assets/images/auth-background.jpg")}
+        source={images[bg_image_index]}
         style={styles.bgImage}
       >
         <Image
           source={require("../assets/images/logo_dark_sh.png")}
           resizeMode="contain"
-          style={styles.splashImage}
+          style={styles.logo}
         />
         <Text style={styles.title}>Continue with Google:</Text>
         <FontAwesome.Button
@@ -44,37 +58,6 @@ export default function LoginRegisterScreen({
         </FontAwesome.Button>
       </ImageBackground>
     </View>
-    // <View style={styles.container}>
-    //   <View style={styles.splash}>
-    //     <ImageBackground
-    //       source={require("../assets/images/auth-background.jpg")}
-    //       resizeMode="cover"
-    //       style={styles.image}
-    //     >
-    //       <Image
-    //         source={require("../assets/images/logo_dark.png")}
-    //         resizeMode="contain"
-    //         style={styles.splashImage}
-    //       />
-    //     </ImageBackground>
-    //   </View>
-    //   <View style={styles.content}>
-    //     <Text style={styles.title}>Continue with Google:</Text>
-    //     <FontAwesome.Button
-    //       name="google"
-    //       backgroundColor="#FFFFFF"
-    //       color="#757575"
-    //       onPress={googleAuth}
-    //       iconStyle={{ marginRight: "15%" }}
-    //       size={25}
-    //       style={styles.google}
-    //     >
-    //       Login with Google
-    //     </FontAwesome.Button>
-    //   </View>
-    //   {/* Use a light status bar on iOS to account for the black space above the modal */}
-    //   <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-    // </View>
   );
 }
 
@@ -97,42 +80,25 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    // padding: 20,
     alignItems: "center",
     borderWidth: 1,
     // borderColor: "green",
     backgroundColor: "black",
-  },
-  content: {
-    flex: 1,
-    borderWidth: 1,
-    // borderColor: "grey",
-    padding: 10,
   },
   google: {
     textAlign: "center",
     borderWidth: 1,
     // borderColor: "red",
   },
-  splash: {
-    flex: 2,
-    width: "100%",
-    borderWidth: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    // borderColor: "grey",
-  },
-  splashImage: {
+  logo: {
     // flex: 1,
     alignItems: "center",
     marginLeft: 10,
-    // marginTop: "30%",
     // borderWidth: 1,
     borderColor: "green",
     width: "85%",
   },
   title: {
-    // flex: 1,
     fontSize: 22,
     fontWeight: "bold",
     // borderWidth: 1,
@@ -141,7 +107,7 @@ const styles = StyleSheet.create({
     letterSpacing: 0,
     textShadowColor: "rgba(0, 0, 0, 0.75)",
     textShadowOffset: { width: 1, height: 1 },
-    textShadowRadius: 7,
+    textShadowRadius: 10,
     // borderColor: "red",
   },
 });
