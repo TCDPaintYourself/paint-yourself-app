a95aa2701d72ab019436f2782a6fb20a064b2a6a
moreplatesmoredates <nugentd2@tcd.ie>
2022-02-10 13:33:40 +0000

    merged master
    Login register screen

--- screens/NewProjectScreen.tsx
+++ screens/NewProjectScreen.tsx
@@ -1,3 +1,4 @@
+import {useState} from "react"
 import { StatusBar } from 'expo-status-bar'
 import { Platform, StyleSheet } from 'react-native'
 import { Text, View } from 'components/Themed'
@@ -5,14 +6,19 @@ import ThemePicker from 'components/ThemePicker'
 import Button from 'components/Button'
 
 export default function NewProjectScreen() {
+  const [projectTheme, setProjectTheme] = useState()
   return (
     <View style={styles.container}>
       <View style={{ alignItems: 'center' }}>
         <Text style={styles.title}>Select photo to style</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
         <View style={styles.buttonDiv}>
-          <Button onPress={upload} title="Upload" />
-          <Button onPress={openCamera} title="Take Photo" />
+          <View style={styles.buttonMargin}>
+            <Button onPress={upload} title="Upload" variant="primary" />
+          </View>
+          <View style={styles.buttonMargin}>
+            <Button onPress={openCamera} title="Take Photo" variant="primary" />
+          </View>
         </View>
         <Text style={styles.themeText}>Select Theme</Text>
       </View>
@@ -53,5 +59,9 @@ const styles = StyleSheet.create({
   buttonDiv: {
     display: 'flex',
     flexDirection: 'row',
+  },
+  buttonMargin: {
+    marginRight: 8,
+    marginLeft: 8,
   },
 })
