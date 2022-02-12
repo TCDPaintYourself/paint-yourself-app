022b2300c40b2b2fa9510611063e80753bd9afa8
ClaireCassidy <clairecassidy0@hotmail.com>
2022-02-12 13:51:10 +0000

    profile header styling

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Button, ImageBackground } from "react-native";
+import { StyleSheet, Button, ImageBackground, Image } from "react-native";
 import { Text, View } from "components/Themed";
 import { RootTabScreenProps } from "types";
 import { FontAwesome } from "@expo/vector-icons";
@@ -22,16 +22,6 @@ export default function ProfileScreen({
           source={require("../assets/images/temp/cover_photo_temp.jpg")}
           style={styles.coverImage}
         >
-          {/* <FontAwesome5.Button
-            name="images"
-            backgroundColor="#FFFFFF"
-            color="#757575"
-            onPress={updateCoverPhoto}
-            // iconStyle={{ marginRight: "15%" }}
-            size={20}
-            style={styles.updateCoverPhotoButton}
-          ></FontAwesome5.Button> */}
-
           <View style={styles.updateCoverPhotoButtonContainer}>
             <FontAwesome5
               name="images"
@@ -44,7 +34,23 @@ export default function ProfileScreen({
         </ImageBackground>
       </View>
       <View style={styles.content}>
-        <Text>Hello</Text>
+        <View style={styles.profileHeader}>
+          <Text style={styles.googleName} numberOfLines={1}>
+            [Google Name]
+          </Text>
+          <View style={styles.profileImgContainer}>
+            <Image
+              source={require("../assets/images/temp/google_profile_pic_temp.jpg")}
+              resizeMode="cover"
+              style={styles.profileImg}
+            />
+          </View>
+          <Text style={styles.numStylegans} numberOfLines={1}>
+            <Text style={{ fontWeight: "bold" }}> N </Text>
+            <Text> StyleGans </Text>
+          </Text>
+        </View>
+        {/* <Text>Hello</Text> */}
       </View>
     </View>
     // <View style={styles.container}>
@@ -75,6 +81,8 @@ const styles = StyleSheet.create({
   },
   content: {
     flex: 4,
+    // flexDirection: "row",
+    width: "100%",
     borderColor: "yellow",
     borderWidth: 1,
   },
@@ -90,6 +98,46 @@ const styles = StyleSheet.create({
     borderColor: "green",
     borderWidth: 1,
     // justifyContent: "center",
+  },
+  googleName: {
+    flex: 37.5,
+    fontSize: 14,
+    textAlign: "center",
+    // paddingLeft: 5,
+    // paddingRight: 5,
+    // borderColor: "cyan",
+    borderWidth: 1,
+  },
+  numStylegans: {
+    flex: 37.5,
+    fontSize: 14,
+    textAlign: "center",
+    // borderColor: "cyan",
+    borderWidth: 1,
+    // paddingLeft: 20,
+    // paddingRight: 20,
+  },
+  profileHeader: {
+    flexDirection: "row",
+    borderColor: "green",
+    borderWidth: 1,
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  profileImg: {
+    width: 80,
+    height: 80,
+    marginTop: -(80 / 2), // 1/2 height
+    borderColor: "white",
+    borderWidth: 3,
+    borderRadius: 50,
+    overflow: "hidden",
+  },
+  profileImgContainer: {
+    flex: 25,
+    alignItems: "center",
+    // borderColor: "red",
+    borderWidth: 1,
   },
   updateCoverPhotoButton: {
     // width: "25%",
@@ -114,20 +162,11 @@ const styles = StyleSheet.create({
     // borderWidth: 1,
     marginBottom: "2%",
     marginRight: "2%",
-    padding: 5,
+    paddingTop: 5,
+    padding: 4,
     borderRadius: 5,
     shadowOpacity: 0.8,
     shadowRadius: 10,
     // elevation: 0.4,
   },
-
-  // title: {
-  //   fontSize: 20,
-  //   fontWeight: "bold",
-  // },
-  // separator: {
-  //   marginVertical: 30,
-  //   height: 1,
-  //   width: "80%",
-  // },
 });
