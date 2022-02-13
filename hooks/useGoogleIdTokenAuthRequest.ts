ca4860e3866e96e31375ef19e795067c966adeaa
Lexes Jan Mantiquilla <mantiquillal@gmail.com>
2022-02-13 13:01:21 +0000

    Add comment

--- hooks/useGoogleIdTokenAuthRequest.ts
+++ hooks/useGoogleIdTokenAuthRequest.ts
@@ -2,6 +2,9 @@ import { useIdTokenAuthRequest } from 'expo-auth-session/providers/google'
 
 import { firebaseClientId } from 'constants/Firebase'
 
+/**
+ * Wrapper hook for id token auth request.
+ */
 export const useGoogleIdTokenAuthRequest = () =>
   useIdTokenAuthRequest({
     clientId: firebaseClientId,
