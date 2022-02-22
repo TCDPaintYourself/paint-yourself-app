ef0858517489eea8c592931765f721b0c9045acf
dabreadman <tany3@tcd.ie>
2022-02-22 10:12:35 +0000

    Can't tell decorator works, but it'd be pogchamp if it does

--- utils/theme_votes.ts
+++ utils/theme_votes.ts
@@ -8,6 +8,7 @@ import {
   doc,
   updateDoc,
   getDocs,
+  onSnapshot,
 } from 'firebase/firestore'
 import { FieldValue } from 'firebase-admin/firestore'
 import { firebaseConfig } from 'constants/Firebase'
@@ -27,24 +28,42 @@ export type ThemeVote = {
 
 export const themeVotesCol = createCollection<ThemeVote>('theme_votes')
 
-export const upvoteTheme = async (theme: string) => {
+export const upvoteTheme = async (theme: string): Promise<void> => {
   const themeVoteRef = doc(themeVotesCol, theme)
   await updateDoc(themeVoteRef, {
     votes: FieldValue.increment(1),
   })
 }
 
-export const downvoteTheme = async (theme: string) => {
+export const downvoteTheme = async (theme: string): Promise<void> => {
   const themeVoteRef = doc(themeVotesCol, theme)
   await updateDoc(themeVoteRef, {
-    votes: FieldValue.increment(1),
+    votes: FieldValue.increment(-1),
   })
 }
 
-export const getThemes = async () => {
+export const getThemes = async (): Promise<ThemeVote[]> => {
   const themeVotesDocs = await getDocs(themeVotesCol)
   let themeVotes: Array<ThemeVote> = []
   themeVotesDocs.docs.forEach((theme) => {
     themeVotes.push(theme.data())
+  })
+  return themeVotes
+}
+
+/**
+ *
+ * @param innerFunc A function that does something to the documents
+ *
+ * Usage
+ *
+ * @onSnapshotDecor
+ * const consoleLog = (docs: DocumentData) => {
+ *  docs.forEach((doc) => console.log(doc.data()))
+ * }
+ */
+export const onSnapShotDecor = async (innerFunc: (doc: DocumentData) => void) => {
+  onSnapshot(doc(firestore, 'theme_votes'), (docs) => {
+    innerFunc(docs)
   })
 }
