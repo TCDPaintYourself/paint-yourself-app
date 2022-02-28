9416db7bdac9505d9a3203f77e613c76800507e6
John Sinclair <sinclairjohn72@gmail.com>
2022-02-28 18:01:27 +0000

    added ipfs todo list

--- utils/ipfs.ts
+++ utils/ipfs.ts
@@ -2,5 +2,13 @@ const { create, globSource } = require('ipfs-http-client')
 
 const client = create()
 
+/*
+TODO
+1. Upload individual image
+2. Add hash to metadata json
+3. Upload folder containing one file named "1"
+4. Return folder ipfs hash
+*/
+
 export const uploadTokenImage = () => {}
 export const uploadMetadataFolder = () => {}
