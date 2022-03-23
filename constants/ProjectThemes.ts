68183c0ef9824a5c591dfb2f5e909c2b95d31c20
ClaireCassidy <clairecassidy0@hotmail.com>
2022-03-23 16:33:38 +0000

    update ProjectThemes.ts to list new categories

--- constants/ProjectThemes.ts
+++ constants/ProjectThemes.ts
@@ -5,6 +5,7 @@ export interface IProjectTheme {
 }
 
 export enum Themes {
+  // artists
   VANGOUGH = 'van-gogh',
   CLAUDEMONET = 'claude-monet',
   REMBRANDT = 'rembrandt',
@@ -13,43 +14,67 @@ export enum Themes {
   DAVINCI = 'da-vinci',
   CARAVAGGIO = 'caravaggio',
   // art styles
+  POPART = 'pop-art',
+  IMPRESSIONISM = 'impressionism',
+  EXPRESSIONISM = 'expressionism',
+  ARTNOUVEAU = 'art-nouveau'
 }
 
 const ProjectTheme: IProjectTheme[] = [
   {
     id: Themes.VANGOUGH,
-    image: require('assets/images/themes/van-gogh.jpg'),
+    image: require('assets/images/themes/van-gogh/self-portrait.jpg'),
     name: 'Van Gogh',
   },
   {
     id: Themes.CLAUDEMONET,
-    image: require('assets/images/themes/claude-monet.jpg'),
+    image: require('assets/images/themes/claude-monet/water-lillies.jpg'),
     name: 'Claude Monet',
   },
   {
     id: Themes.REMBRANDT,
-    image: require('assets/images/themes/rembrandt.jpg'),
+    image: require('assets/images/themes/rembrandt/the-storm.jpg'),
     name: 'Rembrandt',
   },
   {
     id: Themes.WHISTLER,
-    image: require('assets/images/themes/whistler.jpg'),
+    image: require('assets/images/themes/whistler/the-gentle-art-of-making-enemies.jpg'),
     name: 'Whistler',
   },
   {
     id: Themes.PICASSO,
-    image: require('assets/images/themes/picasso.jpg'),
+    image: require('assets/images/themes/picasso/les-femmes-dalgiers.jpg'),
     name: 'Picasso',
   },
   {
     id: Themes.DAVINCI,
-    image: require('assets/images/themes/da-vinci.jpg'),
+    image: require('assets/images/themes/da-vinci/mona-lisa.jpg'),
     name: 'Da Vinci',
   },
   {
     id: Themes.CARAVAGGIO,
-    image: require('assets/images/themes/caravaggio.jpg'),
+    image: require('assets/images/themes/caravaggio/self-portrait.jpg'),
     name: 'Caravaggio',
+  },
+  {
+    id: Themes.POPART,
+    image: require('assets/images/themes/pop-art/warhol_marilyn.jpg'),
+    name: 'Pop-Art',
+  },
+  {
+    id: Themes.IMPRESSIONISM,
+    image: require('assets/images/themes/impressionism/afremov_sleeping-city.jpeg'),
+    name: 'Impressionism',
+  },
+  {
+    id: Themes.EXPRESSIONISM,
+    image: require('assets/images/themes/expressionism/munch_the-scream.jpg'),
+    name: 'Expressionism',
+  },
+  {
+    id: Themes.ARTNOUVEAU,
+    image: require('assets/images/themes/art-nouveau/mucha_daydream.png'),
+    name: 'Art-Nouveau',
   },
 ]
 
