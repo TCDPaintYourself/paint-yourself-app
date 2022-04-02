dc9998b23042a5cf81c545f3a34a7c4100da448e
ClaireCassidy <clairecassidy0@hotmail.com>
2022-04-02 17:54:42 +0100

    add handler to upload style image and return

--- screens/ChooseStyleScreen.tsx
+++ screens/ChooseStyleScreen.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react'
-import { StyleSheet, Dimensions, ActivityIndicator } from 'react-native'
+import { StyleSheet, Dimensions, Image, ActivityIndicator } from 'react-native'
 import * as FileSystem from 'expo-file-system'
 import SnackBar from 'react-native-snackbar-component'
 import type { NativeStackScreenProps } from '@react-navigation/native-stack'
 import 'react-native-get-random-values'
 import { v4 as uuidv4 } from 'uuid'
-
+import * as ImgPicker from 'expo-image-picker'
 import { Text, View } from 'components/Themed'
 import Button from 'components/Button'
 import ThemePicker from 'components/ThemePicker'
@@ -48,6 +48,90 @@ export default function ChooseStyleScreen({ route, navigation }: Props) {
     navigation.navigate('StyleFolderModal', { projectTheme: projectTheme, inputImage: inputImage })
   }
 
+  const handleUpload = async () => {
+    // choose image from gallery
+    let imageResponse = await ImgPicker.launchImageLibraryAsync({
+      mediaTypes: ImgPicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [9, 16], // 8x10 portrait
+      // aspect: [4, 3],
+      quality: 1,
+    })
+
+    if (!imageResponse.cancelled) {
+      // write the new uri to async storage:
+      try {
+        // await AsyncStorage.setItem(AS_KEYS.coverPhotoKey, imageResponse.uri)
+        console.log(imageResponse.uri)
+
+        // send to server
+        const filename = inputImage.split('/').pop()
+        const filetype = filename?.split('.').pop()
+
+        const styleImageUri = Image.resolveAssetSource(imageResponse).uri
+
+        let styleFilename: string | undefined = styleImageUri.split('?')[0]
+        styleFilename = styleFilename.split('/').pop()
+        const styleFiletype = styleFilename?.split('.').pop()
+
+        // setLoading(true)
+
+        const authToken = await user?.getIdToken()
+
+        const formData = new FormData()
+        // Any type as react-native as a custom FormData implementation.
+        formData.append('input_image', { uri: inputImage, name: filename, type: `image/${filetype}` } as any)
+
+        // append reference image info
+        formData.append('reference_image', {
+          uri: styleImageUri,
+          name: styleFilename,
+          type: `image/${styleFiletype}`,
+        } as any)
+
+        let response = null
+        try {
+          response = await fetch(
+            `http://paint-yourself.uksouth.cloudapp.azure.com:8080/styled-images`, // omit project theme
+            {
+              method: 'POST',
+              headers: {
+                Authorization: `Bearer ${authToken}`,
+                'Content-Type': 'multipart/form-data',
+              },
+              body: formData,
+            }
+          )
+        } catch (error: any) {
+          setLoading(false)
+          console.log(error)
+          console.log('Error sending image')
+
+          return
+        }
+
+        const imageBlob = await response.blob()
+        // Remove metadata.
+        const imageBase64 = (await blobToBase64(imageBlob)).split(',').pop()
+
+        if (!imageBase64) {
+          return
+        }
+
+        const imageUri = `${FileSystem.cacheDirectory}/${uuidv4()}.${filetype}`
+        await FileSystem.writeAsStringAsync(imageUri, imageBase64, {
+          encoding: FileSystem.EncodingType.Base64,
+        })
+
+        // setLoading(false)
+
+        navigation.navigate('FinishedArtScreen', { image: imageUri, theme: projectTheme.id })
+      } catch (error) {
+        console.log('Unexpected error uploading style')
+      }
+    }
+  }
+
   return (
     <View style={styles.container}>
       <SnackBar
@@ -72,8 +156,7 @@ export default function ChooseStyleScreen({ route, navigation }: Props) {
             <Text style={styles.uploadStyleText}> OR </Text>
             <Button
               disabled={!projectTheme}
-              onPress={handleContinue}
-              // style={!projectTheme ? styles.disabledButton : styles.continueButton}
+              onPress={handleUpload}
               style={styles.uploadStyleButton}
               title="Upload Your Own"
             />
